199daf888f45a878df0c5dee6aa34266eee38126
julienneknowles <julienneknowles@gmail.com>
2024-06-23 17:43:52 -0500

    Appended forecast data to five-day forecast cards

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const day3 = document.getElementById("forecast-card3");
 const day4 = document.getElementById("forecast-card4");
 const day5 = document.getElementById("forecast-card5");
 
+const dateCard1 = document.getElementById("date1");
+const dateCard2 = document.getElementById("date2");
+const dateCard3 = document.getElementById("date3");
+const dateCard4 = document.getElementById("date4");
+const dateCard5 = document.getElementById("date5");
+
 let city;
 let date; 
 let lat;
@@ -55,65 +61,73 @@ fetch(queryURL)
         console.log("Humidity:" +data.list[0].main.humidity);
         console.log("Wind:" +data )
 
-    
- //Add city name, temp and humidty onto main city card       
+   
+ //Add city name, date, temp and humidty onto main city card       
     function addCityData() {    
     const cityTemp = Math.round(data.list[0].main.temp);  
     const cityHumidity = data.list[0].main.humidity;
     const cityWind = data.list[0].wind.speed;
     const cityDate = data.list[0].dt_txt;
+    formatCityDate = cityDate.slice(0,10);
+    cityDateJs = dayjs(formatCityDate).format('ddd, MM/DD/YY');
     
     cityContainerEl.innerHTML = `<p class="title is-5">${cityName}</p> 
                                  <p>${state}</p>
-                                 <p>Date: ${cityDate}</p>
+                                 <p>Date: ${cityDateJs}</p>
                                  <p>Temp: ${cityTemp}°F</p>
                                  <p>Humidity: ${cityHumidity}%</p>
                                  <p>Wind: ${cityWind} MPH</p>`;
 
 
 //Add date, temp, humidity and wind to forecast cards
-    const day1CityTemp = Math.round(data.list[8].main.temp);
-    const day1CityHumidity = data.list[8].main.humidity;
-    const day1CityWind = data.list[8].wind.speed;
-    const day1Date = data.list[8].dt_txt;                   
+    const day1CityTemp = Math.round(data.list[7].main.temp);
+    const day1CityHumidity = data.list[7].main.humidity;
+    const day1CityWind = data.list[7].wind.speed;
+    const day1Date = data.list[7].dt_txt;
+    formatCityDate1 = day1Date.slice(0,10);
+    cityDateJs1 = dayjs(formatCityDate1).format('ddd, MM/DD/YY');                   
                                  
 
-    
-    day1.innerHTML = `<p class="title is-5"> ${day1Date}<p>
-                      <p>Temp: ${day1CityTemp} °F </p> 
+    dateCard1.insertAdjacentHTML ('afterbegin', `<p class=" title is-5"> ${cityDateJs1}<p>`);
+    day1.innerHTML = `<p>Temp: ${day1CityTemp} °F</p> 
                       <p>Humidity: ${day1CityHumidity}% </p>
                       <p>Wind: ${day1CityWind} MPH</p>`;
 
-    const day2CityTemp = Math.round(data.list[16].main.temp);
-    const day2CityHumidity = data.list[16].main.humidity;
-    const day2CityWind = data.list[16].wind.speed;
-    const day2Date = data.list[16].dt_txt;       
+    const day2CityTemp = Math.round(data.list[15].main.temp);
+    const day2CityHumidity = data.list[15].main.humidity;
+    const day2CityWind = data.list[15].wind.speed;
+    const day2Date = data.list[15].dt_txt;
+    formatCityDate2 = day2Date.slice(0,10);
+    cityDateJs2 = dayjs(formatCityDate2).format('ddd, MM/DD/YY');        
 
-    
-    day2.innerHTML = `<p class="title is-5"> ${day2Date}<p>
-                      <p>Temp: ${day2CityTemp}°F</p
+    dateCard2.insertAdjacentHTML ('afterbegin', `<p class=" title is-5"> ${cityDateJs2}<p>`);
+    day2.innerHTML = `<p>Temp: ${day2CityTemp}°F</p
                       <p>Humidity: ${day2CityHumidity}% </p>
                       <p>Wind: ${day2CityWind} MPH</p>`;
 
                 
 
-    const day3CityTemp = Math.round(data.list[24].main.temp);
-    const day3CityHumidity = data.list[24].main.humidity;
-    const day3CityWind = data.list[24].wind.speed;
-    const day3Date = data.list[24].dt_txt;  
+    const day3CityTemp = Math.round(data.list[23].main.temp);
+    const day3CityHumidity = data.list[23].main.humidity;
+    const day3CityWind = data.list[23].wind.speed;
+    const day3Date = data.list[23].dt_txt; 
+    formatCityDate3 = day3Date.slice(0,10);
+    cityDateJs3 = dayjs(formatCityDate3).format('ddd, MM/DD/YY');  
     
-    day3.innerHTML = `<p class="title is-5"> ${day3Date}<p>
-                      <p>Temp: ${day3CityTemp}°F</p>
+    dateCard3.insertAdjacentHTML ('afterbegin', `<p class=" title is-5"> ${cityDateJs3}<p>`);
+    day3.innerHTML = `<p>Temp: ${day3CityTemp}°F</p>
                       <p>Humidity: ${day3CityHumidity}%</p>
                       <p>Wind: ${day3CityWind} MPH</p>`;
 
-    const day4CityTemp = Math.round(data.list[32].main.temp);
-    const day4CityHumidity = data.list[32].main.humidity;
-    const day4CityWind = data.list[32].wind.speed;
-    const day4Date = data.list[32].dt_txt; 
+    const day4CityTemp = Math.round(data.list[31].main.temp);
+    const day4CityHumidity = data.list[31].main.humidity;
+    const day4CityWind = data.list[31].wind.speed;
+    const day4Date = data.list[31].dt_txt; 
+    formatCityDate4 = day4Date.slice(0,10);
+    cityDateJs4 = dayjs(formatCityDate4).format('ddd, MM/DD/YY'); 
 
-    day4.innerHTML = `<p class="title is-5"> ${day4Date}<p>
-                      <p>Temp: ${day4CityTemp}°F</p>
+    dateCard4.insertAdjacentHTML ('afterbegin', `<p class=" title is-5"> ${cityDateJs4}<p>`);    
+    day4.innerHTML = `<p>Temp: ${day4CityTemp}°F</p>
                       <p>Humidity: ${day4CityHumidity}%</p>
                       <p>Wind: ${day4CityWind} MPH</p>`;  
 
@@ -121,9 +135,11 @@ fetch(queryURL)
     const day5CityHumidity = data.list[39].main.humidity;
     const day5CityWind = data.list[39].wind.speed;
     const day5Date = data.list[39].dt_txt; 
-    
-    day5.innerHTML = `<p class="title is-5"> ${day5Date}<p>
-                      <p>Temp: ${day5CityTemp}°F</p>
+    formatCityDate5 = day5Date.slice(0,10);
+    cityDateJs5 = dayjs(formatCityDate5).format('ddd, MM/DD/YY'); 
+
+    dateCard5.insertAdjacentHTML ('afterbegin', `<p class=" title is-5"> ${cityDateJs5}<p>`);
+    day5.innerHTML = `<p>Temp: ${day5CityTemp}°F</p>
                       <p>Humidity: ${day5CityHumidity}% </p>
                       <p>Wind: ${day5CityWind} MPH </p>`; 
 
